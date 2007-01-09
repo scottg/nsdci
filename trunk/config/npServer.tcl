cb32799d5c7fff0fd4238b67a7982d133a3f81ab
michaelandrewsjr <michaelandrewsjr@daa61702-f724-0410-9921-2736862ba2a7>
2007-01-09 04:03:42 +0000

    Added acl section

--- trunk/config/npServer.tcl
+++ trunk/config/npServer.tcl
@@ -1,7 +1,19 @@
+#
+# Set up the RPC server.
+#
+ns_section "ns/server/$server/module/dci/rpc/server/np"
+    ns_param http 1
+
+ns_section "ns/server/$server/module/dci/rpc/server/np/acl"
+    ns_param allow 127.0.0.1
+
+#
+# Configure Networked Poll.
+#
 ns_section "ns/server/$server/module/dci/np"
     ns_param debug 1
 
 ns_section "ns/server/$server/module/dci/np/server/clients"
-
-ns_section "ns/server/$server/module/dci/rpc/server/np"
-    ns_param http 1
+    # This section has no params but is required.
+    # Legacy client list before HTTP RPC.
+    # See the rpc/server acl section above.
--- trunk/config/nproxyServer.tcl
+++ trunk/config/nproxyServer.tcl
@@ -1,14 +1,22 @@
+# 
+# Set up the RPC server.
+#
 ns_section "ns/server/$server/module/dci/rpc/server/nproxy"
     ns_param http 1
 
+ns_section "ns/server/$server/module/dci/rpc/server/nproxy/acl"
+    ns_param allow 127.0.0.1
+
+#
+# Configure nproxy.
+#
 ns_section "ns/server/$server/module/dci/nproxy"
     ns_param debug 1
 
 ns_section "ns/server/$server/module/dci/nproxy/servers"
     ns_param $server $server
 
-# ns_section "ns/server/$server/module/dci/nproxy/server/$server"
-#    ns_param handshakeName customHandshakeName
-
 ns_section "ns/server/$server/module/dci/nproxy/server/$server/clients"
-    # This section has no params but is required
+    # This section has no params but is required.
+    # Legacy client list before HTTP RPC.
+    # See the rpc/server acl section above.
