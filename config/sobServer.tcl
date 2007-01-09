907972731d9f1bb0f9972f398dbdcfd82e8231ea
nathan.folkman <nathan.folkman@daa61702-f724-0410-9921-2736862ba2a7>
2007-01-09 02:15:06 +0000

    Updated to use topology.tcl.

--- config/sobServer.tcl
+++ config/sobServer.tcl
@@ -8,8 +8,15 @@ ns_section "ns/server/$server/module/dci/server"
 ns_section "ns/server/$server/module/dci/rpc/server/nsobc:sob"
     ns_param http 1
 
-ns_section "ns/server/$server/module/dci/rpc/server/nsobc:sob/acl"
-    ns_param allow "127.0.0.1"
+ns_section "ns/server/$server/module/dci/rpc/server/nsobc:sob/acl" 
+    set sobClients [topology.getValue "sobClients"]
+
+    foreach sobClient $sobClients {
+       set address [topology.getValue "httpAddress" $sobClient]
+       set allowed($address) 1
+    }
+    
+    ns_param allow [join [array names allowed] ","]
 
 ns_section "ns/server/$server/module/dci/broadcast"
     ns_param debug 1
